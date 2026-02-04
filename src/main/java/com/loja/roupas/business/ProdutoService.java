10ad9069c84cd895f216e4e135cd3ddb429842dc
Moroni <moronioliveira24@gmail.com>
2026-02-03 21:14:09 -0300

    Aqui eu fiz umas modificçôes adicionando uma metodo ára não deixar um produto ser adiconando mais de uma vez, não aceitar produtos negativos e ordenar os produtos cadastrados por Id em ordem acendente

--- src/main/java/com/loja/roupas/business/ProdutoService.java
+++ src/main/java/com/loja/roupas/business/ProdutoService.java
@@ -3,28 +3,38 @@ package com.loja.roupas.business;
 import com.loja.roupas.infraestructure.entity.Produto;
 import com.loja.roupas.infraestructure.repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProdutoService {
     private final ProdutoRepository repository;
+    //Create
     public Produto salvarNovoProduto(Produto produto){
         if (produto.getPrice() == null || produto.getPrice().compareTo(BigDecimal.ZERO) <= 0){
             throw new RuntimeException("O preço DEVE ser maior que zero!!!");
         }
+        Optional<Produto> produtoExistente = repository.findByName(produto.getName());
+            if (produtoExistente.isPresent()) {
+                throw new RuntimeException("Este produto já existe");
+            }
+
         return repository.save(produto);
     }
+    //Read
     public List<Produto> buscarTodos(){
-        return repository.findAll();
+
+        return repository.findAllByOrderIdAsc();
     }
+    //Delete
     public void deletar(Long id){
     repository.deleteById(id);
     }
+    //Update
     public Produto atualizar(Long id, Produto produtoAtualizado){
         Produto produtoExistente = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Produto não encontrado"));
